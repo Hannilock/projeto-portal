aff7ef17c6ae8c8aa06822b67c02579d1cbe018f
Hannilock <31757372+Hannilock@users.noreply.github.com>
2018-07-31 15:08:18 -0300

    patch 30.07.01

--- php/editInfo.php
+++ php/editInfo.php
@@ -21,13 +21,13 @@
 	<body>
 		<div class="holder" id="edit">
 			<h3>Person Info edition</h3> 
-			<h2>Editing: <?php echo $person[0] ?></h2>
+			<h2 id="edinfo">Editing: <?php echo $person[0] ?></h2>
 			<form action="listing.php" method="POST">
 				Nome: <input type="text" name="name" value="<?php echo $person[0] ?>"/></br>
 				Data de Nascimento: <input type="text" name="birthdate" value="<?php echo $person[1] ?>"/></br>
-				Endereço:<input type="text" name="adress" value="<?php echo $person[2] ?>"/></br>
-				Telefone:<input type="text" name="phone" value="<?php echo $person[3] ?>"/></br>
-				Email:<input type="text" name="email" value="<?php echo $person[4] ?>"/></br>
+				Endereço: <input type="text" name="adress" value="<?php echo $person[2] ?>"/></br>
+				Telefone: <input type="text" name="phone" value="<?php echo $person[3] ?>"/></br>
+				Email: <input type="text" name="email" value="<?php echo $person[4] ?>"/></br>
 				<input type="hidden" name="position" value="<?php echo $position ?>"/>
 				<input class="default-button" type="submit" name="sub" value="enviar edição">
 			</form>
